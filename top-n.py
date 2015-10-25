b25efc9038966efcda65cbd27950fcd53db1a68e
r4yan2 <grazioandre@gmail.com>
2015-10-25 13:02:44 +0100

    Improved top-N with variable shrink

--- top-n.py
+++ top-n.py
@@ -1,34 +1,47 @@
 import csv
+import math
 import operator
+from collections import defaultdict
 
+## Parsing the train.csv
 parsed = list (csv.reader(open('train.csv', 'rb'), delimiter = ','))
+
+## Calculating number of users
+
+nUsers=len(parsed)
+
+## Insert into an hashmap the shrink vaue associated to an item and another hashmap with the normalized rating 
 sum=0
 counter=0
 total={}
-shrink=3
+shrink={}
 del parsed[0]
-lastid=parsed[0][1]
+lastid=int(parsed[0][1])
 for line in parsed:
     line = map (int, line)
     if (lastid!=line[1]):
-        total[lastid]=sum/float(counter+shrink);
+	shrink[lastid]=int(math.fabs(math.log(float(counter)/nUsers)));
+        total[lastid]=sum/float(counter+shrink[lastid]);
         counter=0;
         sum=0;
         lastid=line[1];
     sum=sum+line[2]
     counter=counter+1
 
+## Sorting in descending order the list of items
 sorted_total = dict(sorted(total.items(), key=operator.itemgetter(1), reverse=True))
 top=sorted(sorted_total.items(), key=lambda x:x[1], reverse=True)
 
-from collections import defaultdict
+## Creating the UserEvaluatedList that is the list of items already evaluated by an user
 uel = defaultdict(list)
 for line in parsed:
         line = map (int, line)
         uel[line[0]].append(line[1])
-        
+ 
+## Creating results by filtering out of the top list the items already seen by users 
 submission = list (csv.reader(open('test.csv', 'rb'), delimiter = ','))
 test=[]
+test.append(['userId','testItems'])
 del submission[0]
 for elem in submission:
     count=0
@@ -36,7 +49,6 @@ for elem in submission:
     recommendetions=''
     while count<5:
         if not (top[iterator][0] in uel[int(elem[0])]):
-            #elem.append(top[iterator][0])
             recommendetions=recommendetions+(str(top[iterator][0])+' ')
             iterator=iterator+1
             count=count+1
@@ -44,8 +56,9 @@ for elem in submission:
             iterator=iterator+1
     elem.append(recommendetions)
     test.append(elem)
+
+## Writing Results
 with open ('test.csv', 'w') as fp:
      a = csv.writer(fp, delimiter=',')
-     a.writerows(test)
-        
+     a.writerows(test)       
 fp.close
