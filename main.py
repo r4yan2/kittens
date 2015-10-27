fd2e3e49f7161101ea038cc95913be09c857ed2a
r4yan2 <grazioandre@gmail.com>
2015-10-27 13:20:19 +0100

    improved previous top-N with feature preferences

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import math
 import operator
 from collections import defaultdict
 
+
+
 # Parsing the train.csv
 train = list (csv.reader(open('train.csv', 'rb'), delimiter = ','))
 
@@ -20,17 +22,17 @@ lastid=int(train[0][1])
 for line in train:
     line = map (int, line)
     if (lastid!=line[1]):
-	shrink[lastid]=int(math.fabs(math.log(float(counter)/nUsers)));
+        shrink[lastid]=int(math.fabs(math.log(float(counter)/nUsers)));
         total[lastid]=sum/float(counter+shrink[lastid]);
         counter=0;
         sum=0;
         lastid=line[1];
     sum=sum+line[2]
     counter=counter+1
-
 # Sorting in descending order the list of items
-sortedTotal = dict(sorted(total.items(), key=operator.itemgetter(1), reverse=True))
-topN=sorted(sortedTotal.items(), key=lambda x:x[1], reverse=True)
+#sortedTotal = dict(sorted(total.items(), key=operator.itemgetter(1), reverse=True))
+topN=sorted(total.items(), key=lambda x:x[1], reverse=True)
+topN5=topN[:1666]
 
 # Creating the UserEvaluatedList that is the list of items already evaluated by an user
 
@@ -50,8 +52,8 @@ for elem in test:
     iterator=0
     recommendetions=''
     while count<5:
-        if not (top[iterator][0] in uel[int(elem[0])]):
-            recommendetions=recommendetions+(str(top[iterator][0])+' ')
+        if not (topN[iterator][0] in uel[int(elem[0])]):
+            recommendetions=recommendetions+(str(topN[iterator][0])+' ')
             iterator=iterator+1
             count=count+1
         else:
@@ -71,18 +73,54 @@ for elem in byfeature:
 
 ## User feature avg rating
 ufr={}
+ufc={}
+ufc = defaultdict(lambda: 0.0, ufc)
 ufr = defaultdict(lambda: 0.0, ufr)
 for i in train:
     i=map(int, i)
     for j in ifl[i[1]]:
         ufr[(i[0],j)]=(ufr[(i[0],j)]+float(i[2]))/2
+        ufc[j]=ufc[j]+1 #TODO non farti esplodere il cervello, inizializza ufc a 0
+        
 
 ## TODO fare la top-N personalizzata con la normalizzazione del voto basata sulle medie valutazioni dell'utente per una certa feature
 
+personalizedTopN={}
+dictTopN5=dict(topN[:2800])
+for user in test:
+    for elem in dictTopN5:
+        personalizedTopN[(int(user[0]),elem)]=dictTopN5[elem]
+        for i in ifl[elem]:
+            personalizedTopN[(int(user[0]),elem)]=personalizedTopN[(int(user[0]),elem)]+ufr[(int(user[0]),i)]-math.log(math.fabs(ufr[(int(user[0]),i)]/ufc[i]-5))
+
+#sortedPersonalized = dict(sorted(personalizedTopN.items(), key=operator.itemgetter(1), reverse=True))
+topNPersonalized=sorted(personalizedTopN.items(), key=lambda x:x[1], reverse=True)
+
+test = list (csv.reader(open('test.csv', 'rb'), delimiter = ','))
+result = []
+result.append(['userId','testItems'])
+del test[0]
+for elem in test:
+    elem=map(int,elem)
+    count=0
+    iterator=0
+    recommendetions=''
+    while count<5:
+        if not (topNPersonalized[iterator][0][0]==elem[0]):
+            iterator=iterator+1
+        elif not (topNPersonalized[iterator][0][1] in uel[elem[0]]):
+            recommendetions=recommendetions+(str(topNPersonalized[iterator][0][1])+' ')
+            iterator=iterator+1
+            count=count+1
+        else:
+            iterator=iterator+1
+    elem.append(recommendetions)
+    result.append(elem)
 
 
 ## Writing Results
 with open ('test.csv', 'w') as fp:
      a = csv.writer(fp, delimiter=',')
-     a.writerows(test)       
+     a.writerows(result)       
 fp.close
+
