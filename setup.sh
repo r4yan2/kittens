86a7b42a6ace2318ebf4017827941a271bc5605a
Andrea Grazioso <grazioandre@gmail.com>
2015-10-30 11:04:51 +0100

    small changes to setup script

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 apt-get -y update
 apt-get -y upgrade
-apt-get install -y git byobu htop zram-config
+apt-get install -y git byobu htop zram-config ipython
 cd /dev/shm/
 git pull https://github.com/r4yan2/kittens
 cd kittens
