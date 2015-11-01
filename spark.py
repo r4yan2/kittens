fda7e753da4cc770856807ed237ee7209c3fc5ab
adelina <adelinadoncheva.georgieva@mail.polimi.it>
2015-11-01 22:10:31 +0100

    stronzo

--- spark.py
+++ spark.py
@@ -1,12 +1,19 @@
-#from pyspark import SparkContext
-#Creating the RDD of the train.csv
-trainRdd=sc.textFile("train.csv").map(lambda line: line.split(","))
-
-#Getting the header of trainRdd
+# Creating the RDD of the train.csv
+trainRdd = sc.textFile("data/train.csv").map(lambda line: line.split(","))
+# Getting the header of trainRdd
 trainFirstRow = trainRdd.first()
 
-#Removing the first line of train, that is userId,itemId,rating
+# Removing the first line of train, that is userId,itemId,rating
 trainRdd = trainRdd.filter(lambda x:x !=trainFirstRow)
 
-#Calculating the number of users that has rated some items
-numberUsers = trainRdd.count()
+# Calculating the number of ratings that has rated some items
+numRatings = trainRdd.count()
+# Calculating the number of distinct users that rated some items
+numUsers = trainRdd.map(lambda r: r[0]).distinct().count()
+# Calculating the number of items that some users rated them
+numMovies = trainRdd.map(lambda r: r[1]).distinct().count()
+print "Got %d ratings from %d users on %d movies." % (numRatings, numUsers, numMovies)
+
+# Creating the RDD of the uel.csv
+#uelRdd = sc.textFile("data/uel.csv").map(lambda line: line.split(","))
+# uelRdd.map(lambda x: map(int,x))
