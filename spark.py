30feae69c9bbe6409d910a5a461c18ef10879d1d
adelina <adelinadoncheva.georgieva@mail.polimi.it>
2015-11-01 23:15:07 +0100

    now we may start to work with RDDs

--- spark.py
+++ spark.py
@@ -4,8 +4,18 @@ trainRdd = sc.textFile("data/train.csv").map(lambda line: line.split(","))
 trainFirstRow = trainRdd.first()
 
 # Removing the first line of train, that is userId,itemId,rating
-trainRdd = trainRdd.filter(lambda x:x !=trainFirstRow)
+trainRdd=trainRdd.filter(lambda x:x !=trainFirstRow)
 
+trainRdd=trainRdd.map(lambda x: [x[0],x[1]])
+
+trainRdd=trainRdd.map(lambda x: map(int,x))
+'''
+# List of user items seen
+trainRdd.groupByKey().map(lambda x: (x[0], list(x[1]))).collect()
+
+#List of users and iterable items
+trainRdd.groupByKey().collect()
+'''
 # Calculating the number of ratings that has rated some items
 numRatings = trainRdd.count()
 # Calculating the number of distinct users that rated some items
@@ -13,7 +23,3 @@ numUsers = trainRdd.map(lambda r: r[0]).distinct().count()
 # Calculating the number of items that some users rated them
 numMovies = trainRdd.map(lambda r: r[1]).distinct().count()
 print "Got %d ratings from %d users on %d movies." % (numRatings, numUsers, numMovies)
-
-# Creating the RDD of the uel.csv
-#uelRdd = sc.textFile("data/uel.csv").map(lambda line: line.split(","))
-# uelRdd.map(lambda x: map(int,x))
