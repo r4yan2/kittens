e9b4bbf18000fd2328978adee43032902919746f
r4yan2 <grazioandre@gmail.com>
2015-11-08 20:48:36 +0100

    huge modification of spark.py to include fallback in case of no similarity between users and improved speed of getRecommendetions

--- spark.py
+++ spark.py
@@ -1,20 +1,126 @@
+#from __future__ import print_function
 import math
 import csv
 import time
+import operator
 from scipy.spatial.distance import cosine
 import numpy as np
-from __future__ import print_function
+from collections import defaultdict
+
+def cos(v1, v2):
+    '''cosine similarity implemented in numpy'''
+    return np.dot(v1, v2) / (np.sqrt(np.dot(v1, v1)) * np.sqrt(np.dot(v2, v2)))
+
+def getUserVector(u):
+    '''Creating the function getUserVector that return the 20k long vector with
+    the user's rating for all items'''
+    listItems = listUserItems(u)
+    itemsList = [0]*moviesNumber
+    for (user,item),v in trainRddMappedValuesCollected:
+        if (user!=u or item not in listItems):
+            continue
+        itemsList[item-1]=v
+    return itemsList
+
+def getEvaluation(u,i):
+    '''Getting the evaluation of a specific film for a user'''
+    return ufvl[(u,i)]
+
+def getRecommendetions(u):
+    '''
+    getRecommendetions(user)
+    work on the user vector to make the cosine similarity
+    if is greater than a threshold then the evaluation
+    of the film is recorded in the possible recommendetions
+    multiplied by the similarity value for that specific user
+    '''
+    v1=listUserItems(u)
+    filmThresh=xrange(6,10)
+    recommendetions=[]
+
+    for u2 in userSet:
+        skip=True
+        if (u2 == u):
+            continue
+        v2=listUserItems(u2)
+        listA=[]
+        listB=[]
+        for i in list(v1):
+            if i in v2:
+                skip=False
+                listA.append(getEvaluation(u,i))
+                listB.append(getEvaluation(u2,i))
+                v2.remove(i)
+        if (skip):
+            similarity = -1
+            globalSimilarityUsers.append( (u,u2) )
+            globalSimilarityValue.append(similarity)
+            continue
+
+        for i in v2:
+            if getEvaluation(u2,i) not in filmThresh:
+                v2.remove(i)
+        #compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)
+        similarity = 1.0-cosine(listA, listB)
+        globalSimilarityUsers.append( (u,u2) )
+        globalSimilarityValue.append(similarity)
+        for i in v2:
+            recommendetions.append((i,getEvaluation(u2,i) * similarity))
+    return recommendetions
+
+def listUserItems(user):
+    '''List of user's seen items'''
+    return itemsList[usersList.index(user)][:]
+
+def numDistinctItems():
+    ''' Getting the number of items that we have in our icm.csv'''
+    icmRdd = sc.textFile("data/icm.csv").map(lambda line: line.split(","))
+    icmFirstRow = icmRdd.first()
+    icmRdd=icmRdd.filter(lambda x:x !=icmFirstRow)
+    numDistinctItems = icmRdd.map(lambda x: map(int,x)).sortByKey(False).keys().first()
+    return numDistinctItems
+
+def padding(u):
+    personalizedTopN={}
+    for i in dictTopN5:
+        i=int(i)
+        personalizedTopN[(u,i)]=math.log(float(dictTopN5[str(i)]))
+        if not i in ifl:
+            continue
+        for f in ifl[i]:
+            if not (ufc[(u,f)]==0 or urc[u]==0 or u not in urc or (u,f) not in ufc):
+                personalizedTopN[(u,i)]=personalizedTopN[(u,i)]+ufr[(u,f)]/(ufc[(u,f)]/urc[u])
+    topNPersonalized=sorted(personalizedTopN.items(), key=lambda x:x[1], reverse=True)
+    count=0
+    iterator=0
+    recommendetions=''
+    while count<5:
+        if (topNPersonalized[iterator][0][0]==u):
+            if not (topNPersonalized[iterator][0][1] in uel[u]):
+                recommendetions=recommendetions+(str(topNPersonalized[iterator][0][1])+' ')
+                count=count+1
+        iterator=iterator+1
+    return recommendetions
 
 def resultWriter(result):
-    # Writing Results
+    ''' Writing Results '''
     with open ('data/result.csv', 'w') as fp:
         writer = csv.writer(fp, delimiter=',')
         writer.writerow(["userId,testItems"])
         writer.writerows(result)
     fp.close
 
-def trainRddLoader():
+def stats():
+    # Calculating the number of ratings that of the items
+    numRatings = trainRddLoader().count()
+    # Calculating the number of distinct users that rated some items
+    numUsers = trainRddLoader().map(lambda r: r[0]).distinct().count()
+    # Calculating the number of items that some users rated them
+    numMovies = trainRddLoader().map(lambda r: r[1]).distinct().count()
+    # Print stats
+    print "Got %d ratings from %d users on %d movies. Total items %d" % (numRatings, numUsers, numDistinctItems())
 
+def trainRddLoader():
     # Creating the RDD of the train.csv
     trainRdd = sc.textFile("data/train.csv").map(lambda line: line.split(","))
     # Getting the header of trainRdd
@@ -30,109 +136,6 @@ def userSetLoader():
     userSet = userSet.filter(lambda x:x !=userSetFirstRow).keys().map(lambda x: int(x)).collect()
     return userSet
 
-def stats():
-    # Calculating the number of ratings that of the items
-    numRatings = trainRddLoader().count()
-    # Calculating the number of distinct users that rated some items
-    numUsers = trainRddLoader().map(lambda r: r[0]).distinct().count()
-    # Calculating the number of items that some users rated them
-    numMovies = trainRddLoader().map(lambda r: r[1]).distinct().count()
-    # Print stats
-    print "Got %d ratings from %d users on %d movies. Total items %d" % (numRatings, numUsers, numDistinctItems())
-
-def getUserVector(u):
-# Creating the function getUserVector that returns the vector with the user's rated items
-    listItems = listUserItems(u)
-    itemsList = [0]*moviesNumber
-    for (user,item),v in trainRddMappedValuesCollected:
-        if (user!=u or item not in listItems):
-            continue
-        itemsList[item-1]=v
-    return itemsList
-
-def getEvaluation(u,i):
-    for (user,item),v in trainRddMappedValuesCollected:
-        if (user == u and item == i):
-            return v
-
-
-def getRecommendetions(u):
-    '''
-    getRecommendetions(user)
-    work on the user vector to make the cosine similarity
-    if is greater than a threshold then the evaluation
-    of the film is recorded in the possible recommendetions
-    multiplied by the similarity value for that specific user
-    '''
-    v1=listUserItems(u)
-    filmThresh=range(6,10)
-    recommendetions=[]
-
-    for u2 in userSet:
-        skip=True
-        if (u2 == u):
-            continue
-        '''if ((i,u) in globalSimilarityUsers):
-            similarity = globalSImilarityValue(globalSimilarityUsers.index( (u,i) ))
-            if (similarity==-1):
-                globalSimilarityUsers.append( (u,i) )
-                globalSimilarityValue.append(similarity)
-                continue'''
-        v2=listUserItems(u2)
-        listA=[]
-        listB=[]
-        for i in list(v1):
-            if i in v2:
-                skip=False
-
-                listA.append(getEvaluation(u,i))
-                listB.append(getEvaluation(u2,i))
-                v2.remove(i)
-            v1.remove(i)
-        if (skip):
-            similarity = -1
-            globalSimilarityUsers.append( (u,u2) )
-            globalSimilarityValue.append(similarity)
-            continue
-
-        for i in list(v2):
-            if getEvaluation(u2,i) not in filmThresh:
-                v2.remove(i)
-        #compute cosine similarity of v1 to v2: (v1 dot v2)/{||v1||*||v2||)
-        '''sumxx, sumxy, sumyy = 0, 0, 0
-        for j in range(len(v1)):
-            y = v2[j]
-            x = v1[j]
-            if (y == 0):
-                continue
-            if (x == 0):
-                if (y in filmThresh):
-                    film.append( (y,j+1) )
-                continue
-            sumxy = sumxy + x*y'''
-        similarity = cosine(listA, listB)
-        globalSimilarityUsers.append( (u,u2) )
-        globalSimilarityValue.append(similarity)
-        for i in v2:
-            recommendetions.append((i,getEvaluation(u2,i) * similarity))
-    return recommendetions
-
-def cos(v1, v2):
-    return np.dot(v1, v2) / (np.sqrt(np.dot(v1, v1)) * np.sqrt(np.dot(v2, v2)))
-
-def numDistinctItems():
-
-    # Getting the number of items that we have in our icm.csv
-    icmRdd = sc.textFile("data/icm.csv").map(lambda line: line.split(","))
-    icmFirstRow = icmRdd.first()
-    icmRdd=icmRdd.filter(lambda x:x !=icmFirstRow)
-    numDistinctItems = icmRdd.map(lambda x: map(int,x)).sortByKey(False).keys().first()
-    return numDistinctItems
-
-def listUserItems(user):
-    # List of user items seen
-    return itemsList[usersList.index(user)]
-
 globalSimilarityUsers=[]
 globalSimilarityValue=[]
 start_time = time.time()
@@ -142,20 +145,63 @@ result=[]
 # Mapping (user,item) as key and rating as value
 trainRddMappedValuesCollected=trainRdd.map(lambda x: map(int,x)).map(lambda x: (list((x[0],x[1])),x[2])).collect()
 
+train = list (csv.reader(open('data/train.csv', 'rb'), delimiter = ','))
+del train[0] #deletion of the string header
+ufvl=dict((((x[0], x[1]), x[2])) for x in map(lambda x: map(int,x),train[1:]))
+
 itemsList=trainRdd.map(lambda x: [x[0],x[1]]).groupByKey().map(lambda x: (int(x[0]), map(int,list(x[1])))).values().collect()
 usersList=trainRdd.map(lambda x: [x[0],x[1]]).groupByKey().map(lambda x: (int(x[0]), map(int,list(x[1])))).keys().collect()
 
 moviesNumber=numDistinctItems()
+# Creating the UserEvaluatedList: the list of items already evaluated by an user
+uel = defaultdict(list)
+for line in train:
+        line = map (int, line)
+        uel[line[0]].append(line[1])
+# Parsing the item feature list
+byfeature = list(csv.reader(open('data/icm.csv', 'rb'), delimiter = ','))
+del byfeature[0]
+ifl = {}
+for elem in byfeature:
+    elem = map (int, elem)
+    if not (elem[0]) in ifl:
+        ifl[elem[0]]=[]
+    ifl[elem[0]].append(elem[1])
+
+# User feature avg rating
+ufr={}
+ufc={}
+urc={}
+ufc = defaultdict(lambda: 0.0, ufc)
+ufr = defaultdict(lambda: 0.0, ufr)
+urc = defaultdict(lambda: 0,urc)
+for elem in train:
+    elem=map(int, elem)
+    u=elem[0]
+    i=elem[1]
+    r=elem[2]
+    urc[u]=urc[u]+1
+    if not i in ifl:
+        continue
+    for f in ifl[i]:
+        ufr[(u,f)]=(ufr[(u,f)]+float(r))/2
+        ufc[(u,f)]=ufc[(u,f)]+1
+
+dictTopN5=dict(list(csv.reader(open('data/topN.csv', 'rb'), delimiter = ',')))
 
 print time.time()-start_time
 print  "Making the recommendetions"
 userSet=userSetLoader()
+loop_time=time.time()
 for user in userSet:
-    print("Completion percentage"+(float(userSet.index(user)*100)/len(userSet)),end='\r')
+    print "Completion percentage %f, increment %f" % (float(userSet.index(user)*100)/len(userSet),time.time()-loop_time)
+    loop_time=time.time()
     recommend=''
     recommendetions=sorted(getRecommendetions(user), key=lambda x:x[1], reverse=True)[:5]
     for i,v in recommendetions:
         recommend=recommend+(str(i)+' ')
+    if (len(recommendetions)<5):
+        recommend=padding(user)
     elem=[]
     elem.append(user)
     elem.append(recommend)
