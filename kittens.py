02feee6e2e1de2a7b5bc05dc260b370cf119e800
adelina <adelinadoncheva.georgieva@mail.polimi.it>
2015-11-09 23:55:34 +0100

    now space between operators as = is added

--- kittens.py
+++ kittens.py
@@ -3,8 +3,8 @@ import math
 import csv
 import time
 import operator
-from scipy.spatial.distance import cosine
 import numpy as np
+from scipy.spatial.distance import cosine
 from collections import defaultdict
 
 def cos(v1, v2):
@@ -13,34 +13,34 @@ def cos(v1, v2):
 
 def getUserAvgRating():
     '''User avg rating'''
-    count={}
+    count = {}
     count = defaultdict(lambda: 0,count)
     for elem in train:
         u=elem[0]
         v=elem[2]
-        avgRating[u] = (avgRating[u]*count[u]+float(v))/(count[u]+1)
-        count[u]=count[u]+1
+        avgRating[u] = (avgRating[u] * count[u] + float(v)) / (count[u] + 1)
+        count[u] = count[u] + 1
 
 def getUserHistoryExtendedVector(u):
     '''Creating the function getUserVector that return the 20k long vector with
     the user's rating for all items'''
     listItems = listUserItems(u)
-    itemsList = [0]*moviesNumber
+    itemsList = [0] * moviesNumber
     for (user,item),value in trainRddMappedValuesCollected:
-        if (user!=u or item not in listItems):
+        if (user != u or item not in listItems):
             continue
-        itemsList[item-1]=value
+        itemsList[item - 1] = value
     return itemsList
 
 def getEvaluation(u,i):
     '''Getting the evaluation of a specific film for a user'''
-    return ufvl[(u,i)]
+    return ufvl[ (u,i) ]
 
 def getRecommendetions(u):
     '''
     getRecommendetions(user)
-    work on the user vector to make the cosine similarity
-    if is greater than a threshold then the evaluation
+    works on the user vector to make the similarity
+    if it is greater than a threshold then the evaluation
     of the film is recorded in the possible recommendetions
     multiplied by the similarity value for that specific user
     '''
@@ -79,32 +79,32 @@ def get_topN():
     lastid = int(train[0][1])
     for line in train:
         line = map (int, line)
-        if (lastid!=line[1]):
-            variableShrink = int(math.fabs(math.log(float(counter)/nUsers)))
-            total[lastid] = sum/float(counter + variableShrink);
+        if (lastid != line[1]):
+            variableShrink = int(math.fabs(math.log(float(counter) / nUsers)))
+            total[lastid] = sum / float(counter + variableShrink);
             counter = 0
             sum = 0
             lastid = line[1]
         sum = sum + line[2]
         counter = counter + 1
     # Sorting in descending order the list of items
-    return sorted(total.items(), key=lambda x:x[1], reverse=True)
+    return sorted(total.items(), key = lambda x:x[1], reverse = True)
 
     '''alternatively get it from the csv'''
     return dict(list(csv.reader(open('data/topN.csv', 'rb'), delimiter = ',')))
 
 def loadUserStats():
     for elem in train:
-        elem=map(int, elem)
-        u=elem[0]
-        i=elem[1]
-        r=elem[2]
-        urc[u]=urc[u]+1
+        elem = map(int, elem)
+        u = elem[0]
+        i = elem[1]
+        r = elem[2]
+        urc[u] = urc[u] + 1
         if not i in ifl:
             continue
         for f in ifl[i]:
-            ufr[(u,f)]=(ufr[(u,f)]+float(r))/2
-            ufc[(u,f)]=ufc[(u,f)]+1
+            ufr[(u,f)] = (ufr[(u,f)] + float(r)) / 2
+            ufc[(u,f)] = ufc[(u,f)] + 1
 
     '''Creating the UserEvaluatedList: the list of items already evaluated by an user'''
     for line in train:
@@ -116,9 +116,8 @@ def loadUserStats():
     for elem in byfeature:
         elem = map (int, elem)
         if not (elem[0]) in ifl:
-            ifl[elem[0]]=[]
+            ifl[elem[0]] = []
         ifl[elem[0]].append(elem[1])
-
 
 def listUserItems(user):
     '''List of user's seen items'''
@@ -128,30 +127,30 @@ def numDistinctItems():
     ''' Getting the number of items that we have in our icm.csv'''
     icmRdd = sc.textFile("data/icm.csv").map(lambda line: line.split(","))
     icmFirstRow = icmRdd.first()
-    icmRdd=icmRdd.filter(lambda x:x !=icmFirstRow)
+    icmRdd = icmRdd.filter(lambda x:x != icmFirstRow)
     numDistinctItems = icmRdd.map(lambda x: map(int,x)).sortByKey(False).keys().first()
     return numDistinctItems
 
 def padding(u):
-    personalizedTopN={}
+    personalizedTopN = {}
     for i in dictTopN5:
-        i=int(i)
-        personalizedTopN[(u,i)]=math.log(float(dictTopN5[str(i)]))
+        i = int(i)
+        personalizedTopN[(u,i)] = math.log(float(dictTopN5[str(i)]))
         if not i in ifl:
             continue
         for f in ifl[i]:
-            if not (ufc[(u,f)]==0 or urc[u]==0 or u not in urc or (u,f) not in ufc):
-                personalizedTopN[(u,i)]=personalizedTopN[(u,i)]+ufr[(u,f)]/(ufc[(u,f)]/urc[u])
-    topNPersonalized=sorted(personalizedTopN.items(), key=lambda x:x[1], reverse=True)
-    count=0
-    iterator=0
-    recommendetions=''
+            if not (ufc[(u,f)] == 0 or urc[u] == 0 or u not in urc or (u,f) not in ufc):
+                personalizedTopN[(u,i)] = personalizedTopN[(u,i)] + ufr[(u,f)] / (ufc[(u,f)] / urc[u])
+    topNPersonalized = sorted(personalizedTopN.items(), key = lambda x:x[1], reverse = True)
+    count = 0
+    iterator = 0
+    recommendetions = ''
     while count<5:
-        if (topNPersonalized[iterator][0][0]==u):
+        if (topNPersonalized[iterator][0][0] == u):
             if not (topNPersonalized[iterator][0][1] in uel[u]):
-                recommendetions=recommendetions+(str(topNPersonalized[iterator][0][1])+' ')
-                count=count+1
-        iterator=iterator+1
+                recommendetions = recommendetions + (str(topNPersonalized[iterator][0][1]) + ' ')
+                count = count + 1
+        iterator = iterator + 1
     return recommendetions
 
 def pearsonCorrelation(u, u2, listA, listB):
@@ -159,8 +158,8 @@ def pearsonCorrelation(u, u2, listA, listB):
     avgu = avgRating[u]
     avgu2 = avgRating[u2]
     for item1, item2 in zip(listA, listB):
-        listNumeratorU = map(lambda listA: listA-avgu, listA)
-        listNumeratorU2 = map(lambda listB: listB-avgu2, listB)
+        listNumeratorU = map(lambda listA: listA - avgu, listA)
+        listNumeratorU2 = map(lambda listB: listB - avgu2, listB)
         numeratorPearson = np.sum([elem1*elem2 for elem1,elem2 in zip(listNumeratorU,listNumeratorU2)])
         listDenU = map(lambda listNumeratorU: listNumeratorU**2, listNumeratorU)
         listDenU2 = map(lambda listNumeratorU2: listNumeratorU2**2, listNumeratorU2)
@@ -171,7 +170,7 @@ def pearsonCorrelation(u, u2, listA, listB):
 def resultWriter(result):
     ''' Writing Results '''
     with open ('data/result.csv', 'w') as fp:
-        writer = csv.writer(fp, delimiter=',')
+        writer = csv.writer(fp, delimiter = ',')
         writer.writerow(["userId,testItems"])
         writer.writerows(result)
     fp.close
@@ -202,14 +201,14 @@ def trainRddLoader():
     '''Getting the header of trainRdd'''
     trainFirstRow = trainRdd.first()
     '''Removing the first line of train, that is userId,iteamId,rating'''
-    trainRdd=trainRdd.filter(lambda x:x !=trainFirstRow)
+    trainRdd = trainRdd.filter(lambda x:x != trainFirstRow)
     return trainRdd
 
 def userSetLoader():
     '''Get of userSet'''
     userSet = sc.textFile("data/test.csv").map(lambda line: line.split(","))
     userSetFirstRow = userSet.first()
-    userSet = userSet.filter(lambda x:x !=userSetFirstRow).keys().map(lambda x: int(x)).collect()
+    userSet = userSet.filter(lambda x:x != userSetFirstRow).keys().map(lambda x: int(x)).collect()
     return userSet
 
 def ufvlLoader():
@@ -219,23 +218,23 @@ def main():
     '''
     main loop
     for all the users in userSet make the recommendetions through getRecommendetions, the output of the function
-    is properly sorted onyl the first top5 elements are considered, eventually it's possible to get padding for the
+    is properly sorted only the first top5 elements are considered, eventually it's possible to get padding for the
     user which getRecommendetions is unable to fill
     Includes also percentage and temporization
     '''
-    loop_time=time.time()
+    loop_time = time.time()
     for user in userSet:
         #sys.stdout.flush()
         #sys.stdout.write("\r %s % %s" % (str(float(userSet.index(user)*100)/len(userSet)),str(time.time()-loop_time)))
-        print "Completion percentage %f, increment %f" % (float(userSet.index(user)*100)/len(userSet),time.time()-loop_time)
-        loop_time=time.time()
-        recommend=''
-        recommendetions=sorted(getRecommendetions(user), key=lambda x:x[1], reverse=True)[:5]
+        print "Completion percentage %f, increment %f" % (float(userSet.index(user)*100) / len(userSet),time.time() - loop_time)
+        loop_time = time.time()
+        recommend = ''
+        recommendetions = sorted(getRecommendetions(user), key = lambda x:x[1], reverse=True)[:5]
         for i,v in recommendetions:
-            recommend=recommend+(str(i)+' ')
+            recommend = recommend+(str(i) + ' ')
         #if (len(recommendetions)<5):
         #    recommend=padding(user)
-        elem=[]
+        elem = []
         elem.append(user)
         elem.append(recommend)
         result.append(elem)
