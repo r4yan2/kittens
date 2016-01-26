317b26644419c9b7dcfe1d4402495a6c77d0d0be
Andrea Grazioso <grazioandre@gmail.com>
2016-01-26 02:20:47 +0100

    CHanged topN script to suite better the main script

--- topN.py
+++ topN.py
@@ -12,15 +12,14 @@ def get_top_n_personalized(u, recommendations):  # recycle from the old recommen
 
     for i, v in top_n:
        personalized_top_n[i] = math.log(v, 10)
-        if len(get_user_evaluation_list(u)) == 2:
-            for f in get_features_list(i):
-                user_feature_rating = get_user_feature_evaluation(u, f)
-                number_feature_rated = get_user_feature_evaluation_count(u, f)
-                number_items_seen = len(get_user_evaluation_list(u))
-                if not ((user_feature_rating == 0) or (number_items_seen == 0) or (
-                            number_feature_rated == 0)):
-                    personalized_top_n[i] += user_feature_rating * (
-                        float(number_feature_rated) / number_items_seen)
+       for f in get_features_list(i):
+           user_feature_rating = get_user_feature_evaluation(u, f)
+           number_feature_rated = get_user_feature_evaluation_count(u, f)
+           number_items_seen = len(get_user_evaluation_list(u))
+           if not ((user_feature_rating == 0) or (number_items_seen == 0) or (
+                       number_feature_rated == 0)):
+               personalized_top_n[i] += user_feature_rating * (
+                   float(number_feature_rated) / number_items_seen)
     top_n_personalized = sorted(personalized_top_n.items(), key=lambda x: x[1], reverse=True)
     count = len(recommendations)
     iterator = 0
