7f65d3adfba99303cc6078bb7738d865b832c22e
Andrea Grazioso (ray) <grazioandre@gmail.com>
2016-01-28 17:03:07 +0100

    changed path of itemSimilarity

--- itemSimilarity.py
+++ itemSimilarity.py
@@ -6,27 +6,26 @@ from item_based import pearson_item_based_correlation
 
 def get_item_similarities(itemI):
 
-
     similarities = []
     
     for itemJ in get_item_set():
-        usersI = get_item_evaluators_list(itemI)[:]  # take the copy of the users that evaluated itemI
-        usersJ = get_item_evaluators_list(itemJ)[:]  # take the copy of the users that evaluated itemJ
+        if itemI != itemJ:
+            usersI = get_item_evaluators_list(itemI)[:]  # take the copy of the users that evaluated itemI
+            usersJ = get_item_evaluators_list(itemJ)[:]  # take the copy of the users that evaluated itemJ
     
-        ratingsItemI = []  # will contain the evaluations of User of the common items with userIterator
-        ratingsItemJ = []  # will contain the evaluations of userIterator of the common items with userIterator
+            ratingsItemI = []  # will contain the evaluations of User of the common items with userIterator
+            ratingsItemJ = []  # will contain the evaluations of userIterator of the common items with userIterator
     
-        for user in usersJ:
-            if user in usersI:
-                ratingsItemI.append(get_evaluation(user,itemI) - get_avg_user_rating(user))
-                ratingsItemJ.append(get_evaluation(user,itemJ) - get_avg_user_rating(user))
-        if not (len(ratingsItemI) == 0):
-            shrink = math.fabs(math.log(float(len(ratingsItemI)) / get_num_users()))
-            similarity = pearson_item_based_correlation(ratingsItemI, ratingsItemJ, shrink)
+            for user in usersJ:
+                if user in usersI:
+                    ratingsItemI.append(get_evaluation(user,itemI) - get_avg_user_rating(user))
+                    ratingsItemJ.append(get_evaluation(user,itemJ) - get_avg_user_rating(user))
+            if not (len(ratingsItemI) == 0):
+                shrink = math.fabs(math.log(float(len(ratingsItemI)) / get_num_users()))
+                similarity = pearson_item_based_correlation(ratingsItemI, ratingsItemJ, shrink)
     
-            if similarity > 0.60:  # taking into consideration only positive and significant similarities
                 similarities.append([itemI,itemJ,similarity])
-    return sorted(similarities, key=lambda x: x[2], reverse=True)[:15]
+    return sorted(similarities, key=lambda x: x[2], reverse=True)[:10]
     """
     with open('data/item_similarities.csv', 'w') as fp:
         writer = csv.writer(fp, delimiter=',')
