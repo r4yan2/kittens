c6ee48be79dfbe6b1dc394fa1c58677678a9440c
kodaline <adelinadoncheva.georgieva@mail.polimi.it>
2017-10-19 13:09:49 +0200

    0.9 to the percentage of track durations

--- recommender.py
+++ recommender.py
@@ -250,7 +250,7 @@ class Recommender:
 
         for track in target_tracks:
             tags = self.db.get_track_tags(track)
-            if track not in playlist_tracks_set and self.db.get_track_duration(track) > 60000 and math.fabs(self.db.get_track_duration(track) - average_playlist_duration) <0.4*average_playlist_duration:
+            if track not in playlist_tracks_set and self.db.get_track_duration(track) > 60000 and math.fabs(self.db.get_track_duration(track) - average_playlist_duration) <0.9*average_playlist_duration:
                 tf_idf_track = []
                 for tag in tags:
                     tf = 1.0 / len(tags)
@@ -328,7 +328,7 @@ class Recommender:
 
         for track in tracks_not_in_playlist:
             tags = self.db.get_track_tags(track)
-            if track not in playlist_tracks_set and self.db.get_track_duration(track) > 60000 and math.fabs(self.db.get_track_duration(track) - average_playlist_duration) <0.4*average_playlist_duration:
+            if track not in playlist_tracks_set and self.db.get_track_duration(track) > 60000 and math.fabs(self.db.get_track_duration(track) - average_playlist_duration) <0.9*average_playlist_duration:
                 tf_idf_track = []
                 for tag in tags:
                     tf = 1.0 / len(tags)
