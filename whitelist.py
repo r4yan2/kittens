45cf3b46b21e27fddda50ebb3bbf1e77132dd57d
Andrea Grazioso <grazioandre@gmail.com>
2017-12-18 15:02:50 +0100

    print message when whitelisting

--- whitelist.py
+++ whitelist.py
@@ -10,6 +10,7 @@ tags = [tag for line in tracks_map.values() for tag in line[4]]
 tags_count = Counter(tags)
 white=[tag for tag, value in tags_count.items() if value < 85 and value > 1]
 
+print "whitelist size", len(white)
 tags_joined = ','.join([str(elem) for elem in white])
 
 file_to = open("data/whitelist", "wb+")
